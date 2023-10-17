0191c179e8b3080b1b11a3780a826532a47b2f40
Mutah3r <evil.programmer.stephen@gmail.com>
2023-10-17 17:42:44 +0600

    refactor code

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,39 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
 import DashboardLayout from "../layouts/DashboardLayout";
-import DashboardHome from "../components/Dashboard/DashboardHome";
-import AccountDetails from "../components/Dashboard/AccountDetails";
-import UserAddress from "../components/Dashboard/UserAddress";
-import Wishlist from "../components/Dashboard/Wishlist";
-import UserOrders from "../components/Dashboard/UserOrders";
 import MainLayout from "../layouts/MainLayout";
+import DashboardHome from "../pages/Dashboard/DashboardHome";
+import UserOrders from "../pages/Dashboard/UserOrders";
+import AccountDetails from "../pages/Dashboard/AccountDetails";
+import UserAddress from "../pages/Dashboard/UserAddress";
+import Wishlist from "../pages/Dashboard/Wishlist";
 
 export const router = createBrowserRouter([
-    {path: "/", element: <MainLayout />, children: [
-        {
-            path: 'dashboard',
-            element: <DashboardLayout />,
-            children: [
-                {
-                    index: true,
-                    element: <DashboardHome />
-                },
-                {
-                    path: '/dashboard/account-details',
-                    element: <AccountDetails />
-                },
-                {
-                    path: '/dashboard/address',
-                    element: <UserAddress />
-                },
-                {
-                    path: '/dashboard/wishlist',
-                    element: <Wishlist />
-                },
-                {
-                    path: '/dashboard/orders',
-                    element: <UserOrders />
-                }
-            ]
-        },
-    ]}
+    {
+        path: "/",
+        element: <MainLayout />
+    },
+    {
+        path: "/dashboard",
+        element: <DashboardLayout />,
+        children: [
+            {
+                path: "/dashboard",
+                element: <DashboardHome />
+            },
+            {
+                path: '/dashboard/orders',
+                element: <UserOrders />
+            },
+            {
+                path: '/dashboard/account-details',
+                element: <AccountDetails />
+            },
+            {
+                path: '/dashboard/address',
+                element: <UserAddress />
+            },
+            {
+                path: '/dashboard/wishlist',
+                element: <Wishlist />
+            }
+        ]
+    }
 ])
