06c83ea1ae971bd46d8015dd3cbcf23dd59af904
Mutah3r <evil.programmer.stephen@gmail.com>
2023-10-17 11:49:31 +0600

    create dashboard branch

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,3 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "../components/Dashboard/Dashboard";
 
-export const router = createBrowserRouter([])
+export const router = createBrowserRouter([
+    {
+        path: '/dashboard',
+        element: <Dashboard />
+    }
+])
