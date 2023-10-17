bc93f39ddec4b2d3782c72a6fa9a93d1ff4cc20d
mamondots <shikderr112@gmail.com>
2023-10-17 17:50:45 +0600

    show products by category

---
+++ src/ProductsPage/ProductCard/ProductCard.jsx
@@ -0,0 +1,57 @@
+import { Rating } from '@smastrom/react-rating';
+import '@smastrom/react-rating/style.css'
+import React from 'react';
+import { AiOutlineShoppingCart,AiFillEye,AiFillHeart } from 'react-icons/ai';
+import { GiNetworkBars } from 'react-icons/gi';
+
+import './ProductCard.css'
+
+const ProductCard = ({ productData }) => {
+    const { id, image, title, newprice, oldprice, review } = productData
+    return (
+        <div className='mx-3 border rounded-md relative main-card cursor-pointer'>
+            <div>
+                <img className='w-full h-full rounded-md' src={image} alt="" />
+                <div className='top-[4%] left-0 absolute icon-set'>
+                    <div className='flex items-center space-x-2 wishCard cursor-pointer'>
+                            <p className='text-xl'><AiFillHeart></AiFillHeart></p>
+                            <p className='px-2 py-1 text-sm bg-[#9D4D4A] rounded-md text-white wishlist'>Add To Wish List</p>
+                    </div>
+
+                    <div className='flex items-center space-x-2 wishCard cursor-pointer'>
+                        <p className='text-xl'><GiNetworkBars></GiNetworkBars></p>
+                        <p className='px-2 py-1 text-sm bg-[#9D4D4A] rounded-md text-white wishlist'>View Details</p>
+                    </div>
+
+                    <div className='flex items-center space-x-2 wishCard cursor-pointer'>
+                        <p className='text-xl'><AiFillEye></AiFillEye></p>
+                        <p className='px-2 py-1 text-sm bg-[#9D4D4A] rounded-md text-white wishlist'>Quick View</p>
+                    </div>
+                </div>
+            </div>
+            <div className='px-4 py-4 cursor-pointer'>
+                
+                <div className='flex items-center space-x-2'>
+                <Rating
+                    style={{ maxWidth: 80 }}
+                    value={review}
+                    readOnly
+                />
+                <p>({review})</p>
+                </div>
+                
+                <h2 className='py-2 border-b'>{title}</h2>
+                <div className='flex items-center space-x-3 py-2'>
+                    <p className='text-[#9D4D4A]'>${newprice}</p>
+                    <p className='text-[#6b6b6b]'><del>${oldprice}</del></p>
+                </div>
+                <div className='bg-[#EFEFEF] space-x-2 rounded-lg flex items-center justify-center py-2 text-[#6e6e6e] font-medium hover:bg-[#9D4D4A] hover:text-white duration-300'>
+                    <AiOutlineShoppingCart></AiOutlineShoppingCart>
+                    <button>ADD TO CART</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,3 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import Products from "../Layout/Products";
+import OurProducts from "../ProductsPage/OurProducts/OurProducts";
 
-export const router = createBrowserRouter([])
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Products></Products>,
+        children: [
+            {
+                path: '/',
+                element: <OurProducts></OurProducts>  
+            }
+        ]
+    }
+])
